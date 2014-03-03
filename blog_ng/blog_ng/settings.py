45382dc62be38ba3155c95e061b344da71192903
Sukhvinder Singh <krramsos@gmail.com>
2014-03-03 16:39:02 +0100

    Implemented list view for posts

--- blog_ng/blog_ng/settings.py
+++ blog_ng/blog_ng/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
-    'blogengine'
+    'blogengine',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Template directory
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
